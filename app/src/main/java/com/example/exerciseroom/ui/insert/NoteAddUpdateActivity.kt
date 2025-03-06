935d71a49ba57b27b33eab8437e3e23169bac8ac
yusuf <yusufnew060@gmail.com>
2025-03-06 14:22:03 +0700

    menghubungkan NoteAddUpdateViewModel dengan NoteAddUpdateActivity

--- app/src/main/java/com/example/exerciseroom/ui/insert/NoteAddUpdateActivity.kt
+++ app/src/main/java/com/example/exerciseroom/ui/insert/NoteAddUpdateActivity.kt
@@ -5,17 +5,43 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
 import com.example.exerciseroom.R
+import com.example.exerciseroom.database.Note
+import com.example.exerciseroom.databinding.ActivityNoteAddUpdateBinding
+import com.example.exerciseroom.helper.ViewModelFactory
 
 class NoteAddUpdateActivity : AppCompatActivity() {
+
+    companion object{
+        const val EXTRA_NOTE = "extra_note"
+        const val ALERT_DIALOG_CLOSE = 10
+        const val ALERT_DIALOG_DELETE = 20
+    }
+
+    private var isEdit = false
+    private var note: Note? = null
+
+    private lateinit var noteAddUpdateViewModel: NoteAddUpdateViewModel
+    private var _activityNoteAddUpdateBinding: ActivityNoteAddUpdateBinding? = null
+    private val binding get() = _activityNoteAddUpdateBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_note_add_update)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        _activityNoteAddUpdateBinding = ActivityNoteAddUpdateBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        noteAddUpdateViewModel = obtainViewModel(this@NoteAddUpdateActivity)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _activityNoteAddUpdateBinding = null
+    }
+
+    private fun obtainViewModel(activity: AppCompatActivity): NoteAddUpdateViewModel {
+        val factory = ViewModelFactory.getInstance(activity.application)
+        return ViewModelProvider(activity, factory).get(NoteAddUpdateViewModel::class.java)
     }
 }
