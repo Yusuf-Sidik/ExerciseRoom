a93560d41197ef07f4b66487351ba528525b7401
yusuf <yusufnew060@gmail.com>
2025-03-06 17:48:52 +0700

    menginisialisasi RecyclerView dan NoteAdapter

--- app/src/main/java/com/example/exerciseroom/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/exerciseroom/ui/main/MainActivity.kt
@@ -1,12 +1,51 @@
 package com.example.exerciseroom.ui.main
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.exerciseroom.R
+import com.example.exerciseroom.databinding.ActivityMainBinding
+import com.example.exerciseroom.helper.ViewModelFactory
+import com.example.exerciseroom.ui.insert.NoteAddUpdateActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private var _activityMainBinding: ActivityMainBinding? = null
+    private val binding get() = _activityMainBinding
+    private lateinit var adapter: NoteAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        _activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        val mainViewModel = obtainViewModel(this@MainActivity)
+        mainViewModel.getAllNotes().observe(this) { noteList ->
+            if (noteList != null) {
+                adapter.setListNotes(noteList)
+            }
+        }
+        
+        adapter = NoteAdapter()
+        binding?.rvNotes?.layoutManager = LinearLayoutManager(this)
+        binding?.rvNotes?.setHasFixedSize(true)
+        binding?.rvNotes?.adapter = adapter
+
+        binding?.fabAdd?.setOnClickListener {
+            val intent = Intent(this@MainActivity, NoteAddUpdateActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun obtainViewModel(activity: AppCompatActivity): MainViewModel {
+        val factory = ViewModelFactory.getInstance(activity.application)
+        return ViewModelProvider(activity, factory).get(MainViewModel::class.java)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _activityMainBinding = null
     }
 }
